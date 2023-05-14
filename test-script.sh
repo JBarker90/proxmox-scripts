23c3f370f4139078d0cfa98eccb393f0babb650b
Jonathan <jonathan.barker09@gmail.com>
2023-05-14 16:43:39 +0000

    added some other lines to test script

--- test-script.sh
+++ test-script.sh
@@ -3,7 +3,13 @@
 echo "===================================="
 echo "This is a Test Script"
 echo "===================================="
+
 echo "Enter your specified directory: "
 read -r -e DIR
-
 echo "$DIR"
+
+echo "Enter the Name of the file you would like created: "
+read -r -e FILE
+
+touch /tmp/"$FILE"
+cp -a /tmp/"$FILE" "$DIR"
