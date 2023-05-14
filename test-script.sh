d22a26a9f2d260e47b54cee6fd55185012e37847
Jonathan <jonathan.barker09@gmail.com>
2023-05-14 16:23:17 +0000

    created test script

---
+++ test-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "===================================="
+echo "This is a Test Script"
+echo "===================================="
+echo "Enter your specified directory: "
+read -r -e DIR
+
+echo "$DIR"
