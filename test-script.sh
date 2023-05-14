c22bb3d297aa456e3e0b06e2d155f4264860936f
Jonathan <jonathan.barker09@gmail.com>
2023-05-14 17:02:43 +0000

    indented if statement

--- test-script.sh
+++ test-script.sh
@@ -6,12 +6,12 @@ echo "===================================="
 
 echo "Enter your specified directory: "
 read -r -e DIR
-if [[ -z "$DIR" ]]; then
-    readlink -f "$DIR"
-else
-    echo "Please specify a directory..."
-    exit 1
-fi
+    if [[ -z "$DIR" ]]; then
+        readlink -f "$DIR"
+    else
+        echo "Please specify a directory..."
+        exit 1
+    fi
 
 echo "Enter the Name of the file you would like created: "
 read -r -e FILE
