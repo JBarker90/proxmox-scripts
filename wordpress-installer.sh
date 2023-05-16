1961b7924643c86dddf9092b9472638938d46ee2
Jonathan <jonathan.barker09@gmail.com>
2023-05-16 03:45:09 +0000

    removed sudo lines

--- wordpress-installer.sh
+++ wordpress-installer.sh
@@ -11,24 +11,24 @@ WP_DIR="/var/www/html/wordpress"
 # Download and extract WordPress
 wget https://wordpress.org/latest.tar.gz -P /tmp
 tar -xzvf /tmp/latest.tar.gz -C /tmp
-sudo mv /tmp/wordpress $WP_DIR
-sudo chown -R www-data:www-data $WP_DIR
-sudo chmod -R 755 $WP_DIR
+mv /tmp/wordpress $WP_DIR
+chown -R www-data:www-data $WP_DIR
+chmod -R 755 $WP_DIR
 
 # Generate new salts
 NEW_SALTS=$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)
 NEW_SALTS=$(echo "$NEW_SALTS" | sed 's/\//\\\//g')
 
 # Update wp-config.php with new salts
-sudo cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
-sudo sed -i "/define('AUTH_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('AUTH_KEY''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('SECURE_AUTH_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('SECURE_AUTH_KEY''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('LOGGED_IN_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('LOGGED_IN_KEY''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('NONCE_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('NONCE_KEY''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('AUTH_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('AUTH_SALT''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('SECURE_AUTH_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('SECURE_AUTH_SALT''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('LOGGED_IN_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('LOGGED_IN_SALT''))" $WP_DIR/wp-config.php
-sudo sed -i "/define('NONCE_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('NONCE_SALT''))" $WP_DIR/wp-config.php
+cp $WP_DIR/wp-config-sample.php $WP_DIR/wp-config.php
+sed -i "/define('AUTH_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('AUTH_KEY''))" $WP_DIR/wp-config.php
+sed -i "/define('SECURE_AUTH_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('SECURE_AUTH_KEY''))" $WP_DIR/wp-config.php
+sed -i "/define('LOGGED_IN_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('LOGGED_IN_KEY''))" $WP_DIR/wp-config.php
+sed -i "/define('NONCE_KEY',/c\$(echo \"$NEW_SALTS\" | grep 'define('NONCE_KEY''))" $WP_DIR/wp-config.php
+sed -i "/define('AUTH_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('AUTH_SALT''))" $WP_DIR/wp-config.php
+sed -i "/define('SECURE_AUTH_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('SECURE_AUTH_SALT''))" $WP_DIR/wp-config.php
+sed -i "/define('LOGGED_IN_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('LOGGED_IN_SALT''))" $WP_DIR/wp-config.php
+sed -i "/define('NONCE_SALT',/c\$(echo \"$NEW_SALTS\" | grep 'define('NONCE_SALT''))" $WP_DIR/wp-config.php
 
 # Create the MySQL database and user
 mysql -u root -p <<MYSQL_SCRIPT
@@ -39,8 +39,8 @@ FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
 # Update wp-config.php with database credentials
-sudo sed -i "s/database_name_here/$DB_NAME/" $WP_DIR/wp-config.php
-sudo sed -i "s/username_here/$DB_USER/" $WP_DIR/wp-config.php
-sudo sed -i "s/password_here/$DB_PASS/" $WP_DIR/wp-config.php
+sed -i "s/database_name_here/$DB_NAME/" $WP_DIR/wp-config.php
+sed -i "s/username_here/$DB_USER/" $WP_DIR/wp-config.php
+sed -i "s/password_here/$DB_PASS/" $WP_DIR/wp-config.php
 
 echo "WordPress has been installed and new salts have been generated."
